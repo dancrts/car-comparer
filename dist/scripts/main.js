6c0c22d651d11d302bc8ef5114c7ad6fb45feee5
dan.crts <oswaldo.daniel@qrsof.com>
2024-04-24 19:11:06 -0600

    added functional JS

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,11 +1,35 @@
-const resizer = document.getElementById(".resizer");
-const sidebar = document.getElementById(".sidebar");
+const resizerDiv = document.getElementById("resizer");
+const resizableDiv = document.getElementById("resizable");
+let viewportWidth = window.innerWidth;
+let xPositionOfPointer, resizableWidth;
 
+function initContentResizer(resizer, resizable) {
+    function onMouseDown(e) {
+        xPositionOfPointer = e.clientX;
+        resizer.style.cursor = "grabbing";
+        let upperWidth = window.getComputedStyle(resizable).width;
+        resizableWidth =  parseInt(upperWidth);
 
-function initContentResizer(resizer, sidebar) {
+        document.addEventListener("mousemove", onMouseMove)
+        document.addEventListener("mouseup", onMouseUp)
+    }
 
-    
+    function onMouseMove(e) {
+        let newXPosition = e.clientX - xPositionOfPointer;
+        let newWidth = resizableWidth + newXPosition;
+
+        if (newWidth < viewportWidth-20 && newWidth > 5) {
+            resizable.style.width = `${newWidth}px`
+        }
+    }
+
+    function onMouseUp() {
+        resizer.style.cursor = "pointer";
+        document.removeEventListener("mousemove", onMouseMove)
+        document.removeEventListener("mouseup", onMouseUp)
+    }
+
+    resizer.addEventListener("mousedown", onMouseDown);
 }
 
-
-initContentResizer(resizer, sidebar);
+initContentResizer(resizerDiv, resizableDiv);
