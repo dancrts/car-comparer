9c0cfb8d2e2033d7620efe2b6f8dad48965ec8e2
dan.crts <oswaldo.daniel@qrsof.com>
2024-04-24 18:27:03 -0600

    added default styles. Working on resize function

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,4 +1,11 @@
-let resizer = document.getElementById(".resizer");
-let sidebar = document.getElementById(".sidebar");
+const resizer = document.getElementById(".resizer");
+const sidebar = document.getElementById(".sidebar");
 
 
+function initContentResizer(resizer, sidebar) {
+
+    
+}
+
+
+initContentResizer(resizer, sidebar);
