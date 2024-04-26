03a723e74b3df706b2d72e78147cdc3e2f7dfb6d
dan.crts <oswaldo.daniel@qrsof.com>
2024-04-26 16:52:34 -0600

    added some quick fixes'

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Este proyecto fue creado para la parte uno de la prueba tecnica de ABEHA.
 
 Fue creado con HTML, Javascript y SCSS debido a la poca complejidad que representaba en terminos de codigo. Por ende, decidi solo utilizar eso en vez de una libreria como React.
+
+No es necesario ninguna accion ni descarga, solo abrir el archivo index.html para visualizarlo.
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -5,27 +5,43 @@ let xPositionOfPointer, resizableWidth;
 
 function onMouseUp() {
     resizerBar.style.cursor = "pointer";
-    document.removeEventListener("mousemove", onMouseMove)
-    document.removeEventListener("mouseup", onMouseUp)
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+    document.removeEventListener("touchend", onMouseUp);
+    document.removeEventListener("touchmove", onMouseMove);
 }
 
 function onMouseDown(event) {
-    xPositionOfPointer = event.clientX;
+    if (event.clientX) {
+        xPositionOfPointer = event.clientX;
+    } else {
+        xPositionOfPointer = event.changedTouches[0].clientX;
+    }
+
     resizerBar.style.cursor = "grabbing";
     let upperWidth = window.getComputedStyle(resizableDiv).width;
-    resizableWidth =  parseInt(upperWidth);
+    resizableWidth = parseInt(upperWidth);
 
-    document.addEventListener("mousemove", onMouseMove)
-    document.addEventListener("mouseup", onMouseUp)
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+    document.addEventListener("touchend", onMouseUp);
+    document.addEventListener("touchmove", onMouseMove);
 }
 
 function onMouseMove(event) {
-    let newXPosition = event.clientX - xPositionOfPointer;
+    let newXPosition;
+    if (event.clientX) {
+        newXPosition = event.clientX - xPositionOfPointer;
+    } else {
+        newXPosition = event.changedTouches[0].clientX - xPositionOfPointer;
+    }
+
     let newWidth = resizableWidth + newXPosition;
 
-    if (newWidth < viewportWidth-26 && newWidth > 16) {
-        resizableDiv.style.width = `${newWidth}px`
+    if (newWidth < viewportWidth - 26 && newWidth > 16) {
+        resizableDiv.style.width = `${newWidth}px`;
     }
 }
 
 resizerBar.addEventListener("mousedown", onMouseDown);
+resizerBar.addEventListener("touchstart", onMouseDown);
